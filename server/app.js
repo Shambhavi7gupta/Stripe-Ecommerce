53e7291d4bcc239b646c171d0dae17becf9043ce
Shambhavi Gupta <90609993+Shambhavi7gupta@users.noreply.github.com>
2024-09-01 17:29:58 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 // Connect to MongoDB
 mongoose
   .connect(
-    "mongodb+srv://shambhavigupta230:0aydYeyAF42hmW8n@cluster0.taa1cmv.mongodb.net/task?retryWrites=true&w=majority&appName=Cluster0
+    "Mongo DB URL
     ",
     {
       useNewUrlParser: true,
